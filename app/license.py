4f0350f8f2c3d151a13acd6f1e6740d6f4458c98
wuzhen <gwuzhen@gmail.com>
2025-11-03 10:43:32 +0800

    Seat License

--- app/license.py
+++ app/license.py
@@ -4,15 +4,14 @@ import json
 import subprocess
 import threading
 import socket
-import platform
 
 import requests
 import tzlocal
 
-from qfluentwidgets import SettingCard, PushSettingCard, InfoBar, InfoBarPosition, TableWidget, PrimaryPushButton
+from qfluentwidgets import SettingCard, InfoBar, InfoBarPosition, TableWidget, PrimaryPushButton, PushButton
 from qfluentwidgets import FluentIcon as FIF
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QFrame, QVBoxLayout, QSpacerItem, QSizePolicy, QTableWidgetItem
+from PySide6.QtWidgets import QFrame, QVBoxLayout, QHBoxLayout, QTableWidgetItem
 
 import app.utils as utils
 from app.config import cfg
@@ -23,8 +22,9 @@ class LicenseWidget(QFrame):
         super().__init__(parent=parent)
         self.loginTuple = loginTuple
         self.url = "https://www.nemopuppet.com/api"
-        self.seat_subscription = []
 
+        self.seats = []
+        self.seatData = None
         self.machineID = None
         self.hostName = socket.gethostname()
 
@@ -33,7 +33,7 @@ class LicenseWidget(QFrame):
 
         self.checkFingerprint()
         self.getSeatLicense()
-        self.fetchSubscriptionData()
+        self.fetchSeatLicense()
 
     def checkFingerprint(self):
         def run(widget, card):
@@ -59,45 +59,62 @@ class LicenseWidget(QFrame):
         thread.start()
         thread.join()
 
-    def fetchSubscriptionData(self, refreshUI=True):
+    def fetchSeatLicense(self):
         recv = requests.get(self.url + "/users/whoami", proxies=utils.get_proxies(), cookies=self.loginTuple[2])
-        subscription = recv.json()['products']
-        self.seat_subscription = [x for x in subscription if x['seats'] > 0]
-        if not refreshUI:
-            return
+        self.seats = sorted(recv.json()['seats'], key=lambda x: x['id'])
 
-        self.tableSubscription.setRowCount(len(self.seat_subscription))
-        for i, sub in enumerate(self.seat_subscription):
-            self.tableSubscription.setItem(i, 0, QTableWidgetItem(sub['product']))
-            self.tableSubscription.setItem(i, 1, QTableWidgetItem(str(sub['seats'])))
-            self.tableSubscription.setItem(i, 2, QTableWidgetItem(str(sub['months'])))
-            self.tableSubscription.setItem(i, 3, QTableWidgetItem(str(sub['softwares'])))
-        self.tableSubscription.resizeColumnToContents(0)
-        self.tableSubscription.resizeColumnToContents(3)
+        self.tableSeats.setRowCount(len(self.seats))
+        for i, seat in enumerate(self.seats):
+            self.tableSeats.setItem(i, 0, QTableWidgetItem(seat['hostname']))
+            self.tableSeats.setItem(i, 1, QTableWidgetItem(str(seat['product'])))
+            self.tableSeats.setItem(i, 2, QTableWidgetItem(str(seat['pack'])))
+            self.tableSeats.setItem(i, 3, QTableWidgetItem(str(seat['months'])))
+            if seat['refresh_at']:
+                time = datetime.datetime.fromisoformat(seat['refresh_at'])
+                self.tableSeats.setItem(i, 4, QTableWidgetItem(str(time.date())))
+            else:
+                self.tableSeats.setItem(i, 4, QTableWidgetItem('N/A'))
+            if seat['expired_at']:
+                time = datetime.datetime.fromisoformat(seat['expired_at'])
+                self.tableSeats.setItem(i, 5, QTableWidgetItem(str(time.date())))
+            else:
+                self.tableSeats.setItem(i, 5, QTableWidgetItem('N/A'))
+            self.tableSeats.setItem(i, 6, QTableWidgetItem(str(seat['fingerprint'])))
+        self.tableSeats.resizeColumnsToContents()
 
     def getSeatLicense(self):
-        self.expired = None
+        self.seatData = None
+        expires = None
 
         license_path = utils.get_license_path()
         if os.path.exists(license_path):
             with open(license_path, "r") as f:
                 data = json.loads(json.load(f)["message"])
                 if data["machine"] == self.machineID:
-                    self.expired = datetime.datetime.fromtimestamp(data["expired_at"])
+                    self.seatData = data
+                    refresh = datetime.datetime.fromtimestamp(data["refresh_at"])
+                    expires = datetime.datetime.fromtimestamp(data["expires_at"])
 
-        if self.expired:
+        if expires:
             self.licenseCard.setContent(self.tr("Machine: ") + self.machineID)
-            ts = self.expired.astimezone(tzlocal.get_localzone())
+            ts = expires.astimezone(tzlocal.get_localzone())
             self.licenseCard.setTitle(
                 self.tr("License")
                 + " | "
-                + self.tr("Expired at ")
+                + self.tr("Expires at ")
                 + ts.strftime("%Y-%m-%d")
             )
+            self.buttonDeactivate.setEnabled(True)
+            if refresh + datetime.timedelta(days=27) < datetime.datetime.now():
+                self.buttonRefresh.setEnabled(True)
+            else:
+                self.buttonRefresh.setEnabled(False)
         else:
             self.licenseCard.setTitle(self.tr("License") + " | " + self.tr("No License Found"))
+            self.buttonDeactivate.setEnabled(False)
+            self.buttonRefresh.setEnabled(False)
 
-    def updateSeatLicense(self):
+    def activateSeatLicense(self):
         if not self.machineID:
             InfoBar.error(
                 title="Failed to get machine ID",
@@ -110,10 +127,10 @@ class LicenseWidget(QFrame):
             )
             return
 
-        if self.tableSubscription.selectedItems() == []:
+        if self.tableSeats.selectedItems() == []:
             InfoBar.error(
-                title="No Subscription Selected",
-                content=self.tr("Please select a subscription first"),
+                title="No Seat Selected",
+                content=self.tr("Please select a seat first"),
                 orient=Qt.Horizontal,
                 isClosable=True,
                 position=InfoBarPosition.TOP,
@@ -122,10 +139,22 @@ class LicenseWidget(QFrame):
             )
             return
 
-        subscription = self.seat_subscription[self.tableSubscription.selectedItems()[0].row()]
-        data = {"hostname": self.hostName, "machine": self.machineID, "subscription": subscription['id']}
+        seat = self.seats[self.tableSeats.selectedItems()[0].row()]
+        if seat['hostname']:
+            InfoBar.error(
+                title="Seat Already Taken",
+                content=self.tr("This seat has been activated on another machine."),
+                orient=Qt.Horizontal,
+                isClosable=True,
+                position=InfoBarPosition.TOP,
+                duration=-1,
+                parent=self,
+            )
+            return
+
+        data = {"hostname": self.hostName, "machine": self.machineID, "seat": seat['id']}
         recv = requests.post(
-            "https://www.nemopuppet.com/api/license/seat/new", params=data, cookies=self.loginTuple[2],
+            "https://www.nemopuppet.com/api/license/seat/activate", params=data, cookies=self.loginTuple[2],
             proxies=utils.get_proxies()
         )
         if not recv.ok:
@@ -146,26 +175,53 @@ class LicenseWidget(QFrame):
             json.dump(license, f, indent=4)
 
         self.getSeatLicense()
-        self.fetchSubscriptionData(refreshUI=False)
-        self.refreshSubscription()
+        self.fetchSeatLicense()
 
-    def refreshSubscription(self):
-        items = self.tableSubscription.selectedItems()
-        if not items:
-            self.licenseCard.button.setEnabled(False)
+    def deactivateSeatLicense(self):
+        license_path = utils.get_license_path()
+        if os.path.exists(license_path):
+            os.remove(license_path)
+        seat_id = self.seatData['seat_id']
+        requests.post(
+            "https://www.nemopuppet.com/api/license/seat/deactivate", params={"seat": seat_id}, cookies=self.loginTuple[2],
+            proxies=utils.get_proxies()
+        )
+        self.getSeatLicense()
+        self.fetchSeatLicense()
+
+    def refreshSeatLicense(self):
+        seat_id = self.seatData['seat_id']
+        recv = requests.post(
+            "https://www.nemopuppet.com/api/license/seat/refresh", params={"seat": seat_id}, cookies=self.loginTuple[2],
+            proxies=utils.get_proxies()
+        )
+        if not recv.ok:
+            InfoBar.error(
+                title="Failed to refresh license",
+                content=f"Response({recv.status_code}): {recv.text}",
+                orient=Qt.Horizontal,
+                isClosable=True,
+                position=InfoBarPosition.TOP,
+                duration=-1,
+                parent=self,
+            )
             return
-        row = items[0].row()
-        sub = self.seat_subscription[row]
-        seats = sub['authored_seats']
-        self.licenseCard.button.setEnabled(sub['seats'] >= len(seats))
 
-        self.tableSeats.setRowCount(len(seats))
-        for i, seat in enumerate(seats):
-            self.tableSeats.setItem(i, 0, QTableWidgetItem(seat['hostname']))
-            self.tableSeats.setItem(i, 1, QTableWidgetItem(seat['applied_at']))
-            self.tableSeats.setItem(i, 2, QTableWidgetItem(seat['expired_at']))
-            self.tableSeats.setItem(i, 3, QTableWidgetItem(seat['fingerprint']))
-        self.tableSeats.resizeColumnsToContents()
+        license = recv.json()
+        license_path = utils.get_license_path()
+        with open(license_path, "w") as f:
+            json.dump(license, f, indent=4)
+
+        self.getSeatLicense()
+        self.fetchSeatLicense()
+
+    def updateLicenseCard(self):
+        if not self.seatData:
+            items = self.tableSeats.selectedItems()
+            if not items:
+                self.buttonActivate.setEnabled(False)
+            else:
+                self.buttonActivate.setEnabled(True)
 
     def setup(self):
         self.layout = QVBoxLayout(self)
@@ -177,33 +233,32 @@ class LicenseWidget(QFrame):
         )
         self.layout.addWidget(self.hostCard)
 
-        spacer = QSpacerItem(20, 20, QSizePolicy.Minimum, QSizePolicy.Expanding)
-        self.layout.addItem(spacer)
-
-        self.tableSubscription = TableWidget(self)
-        self.tableSubscription.setBorderVisible(True)
-        self.tableSubscription.setWordWrap(False)
-        self.tableSubscription.verticalHeader().hide()
-        self.tableSubscription.setColumnCount(4)
-        self.tableSubscription.setHorizontalHeaderLabels(
-            [self.tr("Product"), self.tr("Seats"), self.tr("Months"), self.tr("Softwares")])
-        self.tableSubscription.itemSelectionChanged.connect(self.refreshSubscription)
-        self.layout.addWidget(self.tableSubscription)
+        self.licenseCard = SettingCard(
+            FIF.FINGERPRINT,
+            self.tr("License"),
+            self.tr("Machine: ") + " Unknown",
+        )
+        self.layout.addWidget(self.licenseCard)
 
         self.tableSeats = TableWidget(self)
         self.tableSeats.setBorderVisible(True)
         self.tableSeats.setWordWrap(False)
         self.tableSeats.verticalHeader().hide()
-        self.tableSeats.setColumnCount(4)
-        self.tableSeats.setHorizontalHeaderLabels(["Name", "Applied At", "Expires At", "Machine"])
+        self.tableSeats.setColumnCount(7)
+        self.tableSeats.setHorizontalHeaderLabels(["Name", "Product", "Pack", "Months", "Refresh At", "Expires At", "Machine"])
+        self.tableSeats.itemSelectionChanged.connect(self.updateLicenseCard)
         self.layout.addWidget(self.tableSeats)
 
-        self.licenseCard = PushSettingCard(
-            self.tr("Activate"),
-            FIF.FINGERPRINT,
-            self.tr("License"),
-            self.tr("Machine: ") + " Unknown",
-        )
-        self.licenseCard.clicked.connect(self.updateSeatLicense)
-        self.licenseCard.button.setEnabled(False)
-        self.layout.addWidget(self.licenseCard)
+        buttons = QHBoxLayout()
+        self.buttonRefresh = PushButton(self.tr("Refresh"))
+        self.buttonDeactivate = PrimaryPushButton(self.tr("Deactivate"))
+        self.buttonActivate = PrimaryPushButton(self.tr("Activate"))
+        buttons.addWidget(self.buttonRefresh)
+        buttons.addWidget(self.buttonDeactivate)
+        buttons.addWidget(self.buttonActivate)
+        self.layout.addLayout(buttons)
+
+        self.buttonRefresh.clicked.connect(self.refreshSeatLicense)
+        self.buttonActivate.clicked.connect(self.activateSeatLicense)
+        self.buttonActivate.setEnabled(False)
+        self.buttonDeactivate.clicked.connect(self.deactivateSeatLicense)
